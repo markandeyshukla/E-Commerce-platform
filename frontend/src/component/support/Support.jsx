b558d76f4ac0b48bffaa47cc0e687900c8a21ee3
markandeyshuklae <btw.its.markandey@gmail.com>
2025-09-13 01:06:38 +0530

    support page error solved

--- frontend/src/component/support/Support.jsx
+++ frontend/src/component/support/Support.jsx
@@ -1,5 +1,5 @@
 import './support.css';
-import { Link, useNavigate } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 import { useState } from 'react';
 
 function Support() {
